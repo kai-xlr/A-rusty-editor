c12fcadeda7272e46f94ca013474714169ee6c3f
Kahlil Wallace <kahlil.wallace@protonmail.com>
2025-10-18 13:10:31 -0400

    A1. Added raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self, Read};
+use crossterm::terminal::enable_raw_mode;
+use crossterm::terminal::disable_raw_mode;
+
 fn main() {
-    println!("Hello, world!");
+    for b in io::stdin().bytes() {
+        enable_raw_mode().unwrap();
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
 }
